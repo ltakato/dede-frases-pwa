e8a04b1f5a4972a50a34c26f2e63a8f151da7a08
katako <ltakato@hotmail.com>
2019-07-25 14:38:11 -0300

    feat: setup project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <main>
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+  export default {
+    data: () => ({ })
+  }
+</script>
---
+++ src/components/Quotes.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Frases</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+  export default {
+    data: () => ({ })
+  }
+</script>
